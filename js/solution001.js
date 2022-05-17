a76974a1a69a66e1807637e6224ed702752d70e6
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-05-16 19:24:30 -0700

    cw solutions

---
+++ js/solution001.js
@@ -0,0 +1,9 @@
+function removeChar(hippo){
+ // use let keyord to assing new string variable 
+ let result = hippo.slice(1,-1)
+ console.log(result)
+}
+removeChar('hippo')
+
+//remove first and last character of hippo result expected 'ipp' 
+// examples "hamburger" return "amburg", "buster" return "uste"
---
+++ js/solution002.js
@@ -0,0 +1,15 @@
+//P integer 
+//R return "Even" for even integers return "odd" for odd integers
+//Examples for integer 2 return "even" for integer 5 return "odd
+//p create function taking number 
+//write condtional with modulos 2 to test cases 
+
+function even_or_odd(number) {
+    if(i % 2 === 0){
+        console.log('Even')
+    }else{
+        console.log('Odd')
+    }
+  }
+
+even_or_odd(6)
