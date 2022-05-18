cf978d19c83ba279f342bc7ede98ba628e6a6c1d
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-05-17 21:10:24 -0700

    003

---
+++ js/solution003.js
@@ -0,0 +1,12 @@
+// - take in integers or decimal number
+// - we want to return the opposite (whether negative or positive) of an integer or decimal
+// - (1 / -1) (-9, 9) (4.67, -4.67) 
+//create a function that takes in a number and returns the opposite 
+
+function opposite(number){
+    let opNum = number * -1
+    console.log(opNum)
+}
+opposite(1.97)
+opposite(14)
+opposite(-3)
