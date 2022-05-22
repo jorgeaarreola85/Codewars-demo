8678ec7fa99fbbe378b9e4ca55e06ab6cb050839
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-05-22 12:25:24 -0700

    solution007 completed

---
+++ js/solution007.js
@@ -0,0 +1,14 @@
+// function takes in strings
+// want to return the numberical representation of a string
+//"1234" --> 1234
+//"605"  --> 605
+//"1405" --> 1405
+//"-7" --> -7
+// write a function that takes in a number as a string and returns an ingter of that string. 
+
+const stringToNumber = function(str){
+    // put your code here
+    
+    return Number(str);
+  }
+  console.log(stringToNumber('4'))
