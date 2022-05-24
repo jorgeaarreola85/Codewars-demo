fbb4e9ab04a0d017abf586b4834adfbd9cb7fff9
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-05-24 14:27:07 -0700

    solution009 completed

---
+++ js/solution009.js
@@ -0,0 +1,14 @@
+//take in a year in intger form
+//return the century it is in as an integer 
+//1705 --> 18
+//1900 --> 19
+//1601 --> 17
+//2000 --> 20
+// create a for loop for every 100 years 
+
+function century(year) {
+    // Finish this :)
+    return Math.ceil(year/100);
+  }
+
+  century(2022)
