1fa2e48cd954d9dabdadb1e01bc5a8d0eb0c00fa
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-05-25 18:58:32 -0700

    solution010 solved

---
+++ js/solution010.js
@@ -0,0 +1,9 @@
+//take in a string with spaces
+//return the string with no spaces
+//"robert sucks" return "robertsucks"
+function noSpaces(zebra){
+let zebra2 = zebra.replaceAll(' ','')
+console.log(zebra2)
+}
+
+noSpaces("robert sucks")
