981982ce072aca15af2b763083ca731d3fbd56c9
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-06-03 23:01:18 -0700

    solution015 completed

---
+++ js/solution015.js
@@ -0,0 +1,8 @@
+//take in an array that checks for boolean values and returns the amount of false values as an integer
+//return an integer 
+// for example [true,  true,  true,  false,
+ // true,  true,  true,  true ,
+ // true,  false, true,  false,
+  //true,  false, false, true ,
+  //true,  true,  true,  true ,
+  //false, false, true,  true]
