58d8358f67d4b7b0705246e7897f381b602e4794
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-06-08 21:07:15 -0700

    solution016 completed

--- js/solution015.js
+++ js/solution015.js
@@ -1,4 +1,4 @@
-//take in an array that checks for boolean values and returns the amount of false values as an integer
+//take in an array that checks for boolean values and returns the amount of true values as an integer
 //return an integer 
 // for example [true,  true,  true,  false,
  // true,  true,  true,  true ,
@@ -6,3 +6,17 @@
   //true,  false, false, true ,
   //true,  true,  true,  true ,
   //false, false, true,  true]
+  // answer would be 17
+  // write a function that takes in an array and uses maps to return the number of true values. 
+
+  function countingSheep(arrayOfSheep){
+      let count = 0
+      for (let i = 0; i < arrayOfSheep.length; i++){
+          if(arrayOfSheep[i] === true){
+              count +=1;
+          }
+      }
+      return count;
+  }
+
+  
