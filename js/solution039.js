fbe8096773d5f47b59880bccca2272a31fc266ac
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-07-19 17:50:34 -0600

    solution039

---
+++ js/solution039.js
@@ -0,0 +1,6 @@
+//take in an array of strings 
+// return a single string of the combined strings.
+//example ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+function smash(words) {
+    return words.join(" ");
+  };
