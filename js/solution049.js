647cbe74f2f1bd240bddd9a0697ce48ed43dc252
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-07-31 07:59:52 -0600

    solution 49 completd

---
+++ js/solution049.js
@@ -0,0 +1,18 @@
+//Complete the solution so that it splits the string into pairs of two characters. 
+//If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//Examples:
+
+//* 'abc' =>  ['ab', 'c_']
+//* 'abcdef' => ['ab', 'cd', 'ef']
+
+//take in string
+//return strings of two characters each
+
+function solution(str){
+   let arr = [];
+    for(var i = 0; i < str.length; i += 2){
+      second = str[i+1] || '_';
+      arr.push(str[i] + second);
+    }
+    return arr;
+  }
