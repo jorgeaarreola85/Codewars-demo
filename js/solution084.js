2624babac0169aa459b8b5f3c44dbad0e4634b71
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-09-15 10:03:29 -0700

    solution084 completed

---
+++ js/solution084.js
@@ -0,0 +1,30 @@
+//Our football team finished the championship. The result of each match look like "x:y".
+//Results of all matches are recorded in the collection.
+//
+//For example: ["3:1", "2:2", "0:1", ...]
+//
+//Write a function that takes such collection and counts the points of our team in the championship.
+//Rules for counting points for each match:
+//
+//if x > y: 3 points
+//if x < y: 0 point
+//if x = y: 1 point
+//Notes:
+//
+//there are 10 matches in the championship
+//0 <= x <= 4
+//0 <= y <= 4
+
+function points(games) {
+  // your code here
+
+  return games.reduce((acc, game) => {
+    const [x, y] = game.split(":").map(Number);
+    if (x > y) {
+      acc += 3;
+    } else if (x === y) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
+}
