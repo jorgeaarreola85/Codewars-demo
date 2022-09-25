2b5149c861a85b8be7b9c159cbaaee9e0601610c
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-09-25 14:40:08 -0700

    solution085 completed

---
+++ js/solution085.js
@@ -0,0 +1,12 @@
+//You will be given an array a and a value x.
+//All you need to do is check whether the provided array contains the value.
+//Array can contain numbers or strings. X can be either.
+//Return true if the array contains the value, false if not.
+
+//take in an array of either integers or strings
+//return a boolean
+
+function check(a, x) {
+  // your code here
+  return a.inclides(x);
+}
