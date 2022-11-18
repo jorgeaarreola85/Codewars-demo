dd0066a3537bed9bc3364a069a00c0d96d4cfc8c
jorgeaarreola85 <jorgeaarreola@icloud.com>
2022-11-18 13:39:26 -0800

    changes

---
+++ js/solution094.js
@@ -0,0 +1,18 @@
+//Instructions
+//Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+//Example
+//Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+let capitals = function (word) {
+  // Write your code here
+  let newArr = word.split("");
+};
+
+function map(array1, callback) {
+  let result = [];
+  for (let i = 0; i <= array1.length; i++) {
+    const res = callback(i);
+    result.push(res);
+  }
+}
